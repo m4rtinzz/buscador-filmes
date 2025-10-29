860f9663cbc6edb48e4e780d253e11d6be3b3787
m4rtinzz <contatotin41@gmail.com>
2025-10-29 11:47:24 -0300

    Buscador de filmes com API.

--- buscador-filmes/src/App.tsx
+++ buscador-filmes/src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
 
-function App() {
-
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+    </Router>);
 }
-
-export default App
