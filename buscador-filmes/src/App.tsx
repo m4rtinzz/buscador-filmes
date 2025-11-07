19a924570c2a775480a5655d54767a0bad9ef71f
m4rtinzz <contatotin41@gmail.com>
2025-11-07 10:02:14 -0300

    Aprimorando CSS

--- buscador-filmes/src/App.tsx
+++ buscador-filmes/src/App.tsx
@@ -6,20 +6,22 @@ import './App.css';
 
 export default function App() {
   return (
-    <FavoritesProvider>
-      <Router>
-        <div className='areatitulo'>
-          <div className='titulo'>
-            React Flix
-          </div>
-        </div>
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="*" element={<div>Página não encontrada</div>} />
-        </Routes>
-      </Router>
-    </FavoritesProvider>
+    <div className="app">
+      <FavoritesProvider>
+        <Router>
+          <header className='header'>
+            <h1>
+              React Flix
+            </h1>
+          </header>
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+            <Route path="*" element={<div>Página não encontrada</div>} />
+          </Routes>
+        </Router>
+      </FavoritesProvider>
+    </div>
   );
 }
